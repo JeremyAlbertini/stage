d3a7ad3bc7f940985b9c3e444175e5b4f6db411d
Yannick Kahl <yannick.kahl@epitech.eu>
2025-09-02 10:56:02 +0200

    [~] fixed endless url problem caused by bad navigate usage changed button redirect with navigate to absolute path for create-user

--- site-stage/src/pages/admin.jsx
+++ site-stage/src/pages/admin.jsx
@@ -10,19 +10,20 @@ export default function AdminPage({ users, loadUsers }) {
             <div style={{
                 boxSizing: "border-box",
                 padding: "2rem",
-                backgroundColor:"rgb(61, 33, 114)",
+                backgroundColor:"rgb(108, 60, 197)",
                 marginTop: "60px"
             }}>
                 <h1>Bienvenue sur la page administration</h1>
                 <p>Ici vous pouvez administrer vos agents.</p>
 
                 <button
-                    onClick={() => navigate("create-user")}
+                    onClick={() => navigate("/admin/create-user")}
                     style={{
                         backgroundColor: "rgb(80, 60, 180)",
                         color: "white",
                         fontWeight: "bold",
-                        marginBottom: "20px"
+                        marginBottom: "20px",
+                        
                     }}
                 >
                     Créer un nouvel utilisateur
