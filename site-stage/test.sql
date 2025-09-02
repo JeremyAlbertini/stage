0170cbe60a4daf1462b86003fd0721c6e3f7fa00
Yannick Kahl <yannick.kahl@epitech.eu>
2025-09-02 13:54:10 +0200

    [~] added correct sql db

---
+++ site-stage/test.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS userdata
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci;
+
+USE userdata;
+
+CREATE TABLE IF NOT EXISTS logindata (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    matricule VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS agentdata (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    matricule VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    user_id INT,
+    is_admin INT,
+    FOREIGN KEY (user_id) REFERENCES logindata(id) ON DELETE CASCADE
+);
