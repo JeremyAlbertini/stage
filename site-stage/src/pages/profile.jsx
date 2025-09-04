62641cc4f7b257b93f1c447b235d8add2bfdcba5
JeremyAlbertini <jeremy.albertini@epitech.eu>
2025-09-04 12:03:04 +0200

    [~] change base page of profil

--- site-stage/src/pages/profile.jsx
+++ site-stage/src/pages/profile.jsx
@@ -3,6 +3,7 @@ import LeftBand from "../components/LeftBand";
 import TabGroup from "../components/TabGroup";
 import TabContent from "../components/TabContent";
 import { useEffect, useState } from "react";
+import BasePage from "../components/BasePage";
 
 export default function Profile() {
     const [userData, setUserData] = useState(null);
@@ -43,11 +44,7 @@ export default function Profile() {
     }, []);
 
     return (
-        <div style={{width: "100%", height: "100vh", display: "flex", flexDirection:"column"}}>
-            <Header title="Profil" backgroundColor="rgb(51, 35, 143)" color="white"/>
-            <div style={{ display: "flex", flex: 1, marginTop: '60px'}}>
-                <LeftBand />
-                <div style={{ flex: 1, boxSizing: "border-box", padding: "2rem"}}>
+        <BasePage title="Profile">
                     <h1>Mon Profil</h1>
                     {userData ? (
                         <>
@@ -147,8 +144,6 @@ export default function Profile() {
                     ) : (
                         <p>Chargement des données...</p>
                     )}
-                </div>
-            </div>
-        </div>
+        </BasePage>
     );
 }
